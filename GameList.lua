4134bc527aab5ae35d5e151ef56fe0e742042bc4
TheSyxScripter <87913541+TheSyxScripter@users.noreply.github.com>
2021-07-24 16:23:50 -0700

    ScriptHub Support

---
+++ GameList.lua
@@ -0,0 +1,11 @@
+return {
+    -- Game Selection Structure,
+    -- [game.PlaceIed] = { Script = "game script name", Name = "displayed name"}
+    
+    [155615604] = { Script = "PrisonLife&NinjaLegends.lua",
+    					Name = "SynHax - Prison Life"},
+    [3956818381] = { Script = "PrisonLife&NinjaLegends.lua",
+    					Name = "SynHax - Ninja Legends"},
+    [6872274481] = { Script = "BedWars.lua",
+    					Name = "SynHax - Bedwars"}
+}
