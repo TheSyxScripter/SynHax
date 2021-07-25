b5e5a841699e81c9aba9d1a65b3c406909b0c6ad
TheSyxScripter <87913541+TheSyxScripter@users.noreply.github.com>
2021-07-24 17:21:24 -0700

    Update ScriptLoader.lua

--- ScriptLoader.lua
+++ ScriptLoader.lua
@@ -1,3 +1,6 @@
+-- Gui to Lua
+-- Version: 3.2
+
 -- Instances:
 
 local NEWScriptHub = Instance.new("ScreenGui")
@@ -21,8 +24,6 @@ local Name = Instance.new("TextLabel")
 local Version = Instance.new("TextLabel")
 local DetectedGameText = Instance.new("TextLabel")
 local DetectedGame = Instance.new("TextLabel")
-
-
 
 -- Variables:
 local Returned = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheSyxScripter/SynHax/main/GameList.lua"))() -- Grab Game List
@@ -64,6 +65,7 @@ function CloseHub()
 	delay(0.7, function() NEWScriptHub:ClearAllChildren() NEWScriptHub:Destroy() end)
 end
 
+
 --Properties:
 
 NEWScriptHub.Name = "NEWScriptHub"
@@ -76,21 +78,29 @@ Main.BorderSizePixel = 0
 Main.Position = UDim2.new(0.197320342, 0, 0.062650606, 0)
 Main.Size = UDim2.new(0, 499, 0, 326)
 
+Exit.Name = "Exit"
+Exit.Parent = Main
+Exit.BackgroundColor3 = Color3.fromRGB(221, 221, 221)
+Exit.BorderSizePixel = 0
+Exit.Position = UDim2.new(0.572808921, 0, 0.788343549, 0)
+Exit.Size = UDim2.new(0, 180, 0, 45)
+Exit.ZIndex = 2
+Exit.Font = Enum.Font.GothamSemibold
+Exit.Text = "Exit SynHax"
+Exit.TextColor3 = Color3.fromRGB(0, 0, 0)
+Exit.TextScaled = true
+Exit.TextSize = 14.000
+Exit.TextWrapped = true
 
-TopLabel.Name = "TopLabel"
-TopLabel.Parent = Main
-TopLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TopLabel.BackgroundTransparency = 1.000
-TopLabel.BorderSizePixel = 0
-TopLabel.Position = UDim2.new(0.31556505, 0, 0.0429447889, 0)
-TopLabel.Size = UDim2.new(0, 200, 0, 50)
-TopLabel.Font = Enum.Font.GothamSemibold
-TopLabel.Text = "SyxHax Loader"
-TopLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TopLabel.TextScaled = true
-TopLabel.TextSize = 28.000
-TopLabel.TextWrapped = true
-TopLabel.Visible = false
+Shadow.Name = "Shadow"
+Shadow.Parent = Exit
+Shadow.BackgroundColor3 = Color3.fromRGB(150, 150, 150)
+Shadow.BorderSizePixel = 0
+Shadow.Size = UDim2.new(1, 0, 1, 4)
+
+UICorner.Parent = Shadow
+
+UICorner_2.Parent = Exit
 
 LoadButton.Name = "LoadButton"
 LoadButton.Parent = Main
@@ -120,28 +130,116 @@ UICorner_3.Parent = Shadow_2
 
 UICorner_4.Parent = LoadButton
 
-Exit.Name = "Exit"
-Exit.Parent = Main
-Exit.BackgroundColor3 = Color3.fromRGB(221, 221, 221)
-Exit.BorderSizePixel = 0
-Exit.Position = UDim2.new(0.572808921, 0, 0.788343549, 0)
-Exit.Size = UDim2.new(0, 180, 0, 45)
-Exit.ZIndex = 2
-Exit.Font = Enum.Font.GothamSemibold
-Exit.Text = "Exit SynHax"
-Exit.TextColor3 = Color3.fromRGB(0, 0, 0)
-Exit.TextScaled = true
-Exit.TextSize = 14.000
-Exit.TextWrapped = true
-Exit.Visible = false
+TopLabel.Name = "TopLabel"
+TopLabel.Parent = Main
+TopLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TopLabel.BackgroundTransparency = 1.000
+TopLabel.BorderSizePixel = 0
+TopLabel.Position = UDim2.new(0.31556505, 0, 0.0429447889, 0)
+TopLabel.Size = UDim2.new(0, 200, 0, 50)
+TopLabel.Font = Enum.Font.GothamSemibold
+TopLabel.Text = "SyxHax Loader"
+TopLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TopLabel.TextScaled = true
+TopLabel.TextSize = 14.000
+TopLabel.TextWrapped = true
 
-Exit.MouseButton1Down:Connect(function()
-	CloseHub()
-end)
+UICorner_5.CornerRadius = UDim.new(0, 20)
+UICorner_5.Parent = Main
 
-UICorner.Parent = Shadow
+Desc.Name = "Desc"
+Desc.Parent = Main
+Desc.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Desc.BackgroundTransparency = 1.000
+Desc.BorderSizePixel = 0
+Desc.Position = UDim2.new(0.25373137, 0, 0.263803691, 0)
+Desc.Size = UDim2.new(0, 259, 0, 102)
+Desc.Font = Enum.Font.GothamSemibold
+Desc.Text = "Made By thedogeus#6969 (Still in development!)"
+Desc.TextColor3 = Color3.fromRGB(0, 0, 0)
+Desc.TextScaled = true
+Desc.TextSize = 14.000
+Desc.TextWrapped = true
 
-UICorner_2.Parent = Exit
+DiscordAvatar2.Name = "DiscordAvatar2"
+DiscordAvatar2.Parent = Main
+DiscordAvatar2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+DiscordAvatar2.BackgroundTransparency = 1.000
+DiscordAvatar2.BorderSizePixel = 0
+DiscordAvatar2.Position = UDim2.new(-0.000495405868, 0, 0.216843575, 0)
+DiscordAvatar2.Size = UDim2.new(0, 47, 0, 47)
+DiscordAvatar2.Image = "rbxasset://textures/ui/GuiImagePlaceholder.png"
+
+UICorner_6.CornerRadius = UDim.new(99, 99999)
+UICorner_6.Parent = DiscordAvatar2
+
+Age.Name = "Age"
+Age.Parent = Main
+Age.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Age.BackgroundTransparency = 1.000
+Age.BorderSizePixel = 0
+Age.Position = UDim2.new(0.118177131, 0, 0.161185235, 0)
+Age.Size = UDim2.new(0, 46, 0, 19)
+Age.Font = Enum.Font.GothamSemibold
+Age.Text = "Age:"
+Age.TextColor3 = Color3.fromRGB(0, 0, 0)
+Age.TextSize = 14.000
+
+Age2.Name = "Age2"
+Age2.Parent = Main
+Age2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Age2.BackgroundTransparency = 1.000
+Age2.BorderSizePixel = 0
+Age2.Position = UDim2.new(0.217047125, 0, 0.161185235, 0)
+Age2.Size = UDim2.new(0, 46, 0, 19)
+Age2.Font = Enum.Font.GothamSemibold
+Age2.Text = "N/A"
+Age2.TextColor3 = Color3.fromRGB(0, 0, 0)
+Age2.TextSize = 14.000
+
+Name.Name = "Name"
+Name.Parent = Main
+Name.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Name.BackgroundTransparency = 1.000
+Name.BorderSizePixel = 0
+Name.Position = UDim2.new(0.134447187, 0, 0.244450957, 0)
+Name.Size = UDim2.new(0, 65, 0, 28)
+Name.Font = Enum.Font.GothamBold
+Name.Text = "N/A"
+Name.TextColor3 = Color3.fromRGB(0, 0, 0)
+Name.TextScaled = true
+Name.TextSize = 34.000
+Name.TextWrapped = true
+Name.TextXAlignment = Enum.TextXAlignment.Left
+Name.TextYAlignment = Enum.TextYAlignment.Top
+
+Version.Name = "Version"
+Version.Parent = Main
+Version.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Version.BackgroundTransparency = 1.000
+Version.BorderSizePixel = 0
+Version.Position = UDim2.new(0.622324169, 0, 0.195371181, 0)
+Version.Size = UDim2.new(0, 88, 0, 32)
+Version.Font = Enum.Font.GothamBold
+Version.Text = "Version 1.0"
+Version.TextColor3 = Color3.fromRGB(0, 0, 0)
+Version.TextScaled = true
+Version.TextSize = 14.000
+Version.TextWrapped = true
+
+DetectedGameText.Name = "DetectedGameText"
+DetectedGameText.Parent = Main
+DetectedGameText.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+DetectedGameText.BackgroundTransparency = 1.000
+DetectedGameText.BorderColor3 = Color3.fromRGB(27, 42, 53)
+DetectedGameText.BorderSizePixel = 0
+DetectedGameText.Position = UDim2.new(0.409753382, 0, 0.576687098, 0)
+DetectedGameText.Size = UDim2.new(0, 200, 0, 50)
+DetectedGameText.Font = Enum.Font.GothamSemibold
+DetectedGameText.TextColor3 = Color3.fromRGB(0, 0, 0)
+DetectedGameText.TextScaled = true
+DetectedGameText.TextSize = 14.000
+DetectedGameText.TextWrapped = true
 
 DetectedGame.Name = "DetectedGame"
 DetectedGame.Parent = Main
@@ -157,22 +255,6 @@ DetectedGame.TextColor3 = Color3.fromRGB(0, 0, 0)
 DetectedGame.TextScaled = true
 DetectedGame.TextSize = 14.000
 DetectedGame.TextWrapped = true
-
-DetectedGameText.Name = "DetectedGameText"
-DetectedGameText.Parent = Main
-DetectedGameText.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-DetectedGameText.BackgroundTransparency = 1.000
-DetectedGameText.BorderColor3 = Color3.fromRGB(27, 42, 53)
-DetectedGameText.BorderSizePixel = 0
-DetectedGameText.Position = UDim2.new(0.409753382, 0, 0.576687098, 0)
-DetectedGameText.Size = UDim2.new(0, 200, 0, 50)
-DetectedGameText.Font = Enum.Font.GothamSemibold
-DetectedGameText.TextColor3 = Color3.fromRGB(0, 0, 0)
-DetectedGame.TextScaled = true
-DetectedGame.TextSize = 14.000
-DetectedGame.TextWrapped = true
-DetectedGame.Visible = false
-
 
 -- Opening:
 Main:TweenSize(UDim2.new(0, 596,0, 15), Enum.EasingDirection.InOut, Enum.EasingStyle.Sine, 0.625, true)
@@ -261,4 +343,3 @@ local function PYEGFG_fake_script() -- Name.NameScrip
 	script.Parent.Text = game.Players.LocalPlayer.Name
 end
 coroutine.wrap(PYEGFG_fake_script)()
-
