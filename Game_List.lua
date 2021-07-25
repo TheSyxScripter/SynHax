01fcbc0864f797257fe387d8bc754206fd236cea
TheSyxScripter <87913541+TheSyxScripter@users.noreply.github.com>
2021-07-25 11:44:55 -0700

    Added LOS, and changed file for ninja legends

--- Game_List.lua
+++ Game_List.lua
@@ -4,8 +4,10 @@ return {
     
     [155615604] = { Script = "PrisonLife&NinjaLegends.lua",
     					Name = "SynHax - Prison Life"},
-    [3956818381] = { Script = "PrisonLife&NinjaLegends.lua",
+    [3956818381] = { Script = "NinjaLegends.lua",
     					Name = "SynHax - Ninja Legends"},
+    [3101667897] = { Script = "LOS.lua",
+    					Name = "SynHax - Legends Of Speed"},
     [6872265039] = { Script = "BedWars.lua",
     					Name = "SynHax - Bedwars Lobby"},
     [6872274481] = { Script = "BedWars.lua",
